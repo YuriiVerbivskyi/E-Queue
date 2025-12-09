105a1fd80ec33316c690af35d19756f7ff4e9b7b
rogi <96204825+ihor3310@users.noreply.github.com>
2025-12-09 20:05:45 +0200

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,31 +1,18 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from . import views
-from .views import LoginView, user_profile_page, queue_page, logout_view, login_page
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.Register.as_view(), name='register_api'),
-    path('register-page/', views.register_user, name='register_page'),
-    path('profile/', user_profile_page, name='user_profile_page'),
     path('queues/', views.queues, name='queues'),
-    path('create-room/', views.create_room, name='create_room'),
-    path('join-room/', views.join_room, name='join_room'),
-    path('get-room-entries/', views.get_room_entries, name='get_room_entries'),
-    path('next-student-in-room/', views.next_student_in_room, name='next_student_in_room'),
-    path('nextstudent/', views.next_student, name='next_student'),
-    path('queue/', queue_page, name='queue_page'),
-    path('api/queues/', views.QueueListView.as_view(), name='queue_list_api'),
-    path('api/queues/<int:pk>/', views.QueueDetailView.as_view(), name='queue_detail_api'),
-    path('api/entries/', views.QueueEntryListView.as_view(), name='queue_entries'),
-    path('api/entries/<int:pk>/', views.QueueEntryDetailView.as_view(), name='queue_entry_detail'),
-    path('api/notifications/', views.NotificationListView.as_view(), name='notifications_list'),
-    path('api/notifications/<int:notification_id>/read/', views.mark_notification_as_read, name='mark_read'),
+    path('queues/<int:pk>/', views.QueueDetailView.as_view(), name='queue_detail'),
+    path('entries/', views.QueueEntryListView.as_view(), name='queue_entries'),
+    path('entries/<int:pk>/', views.QueueEntryDetailView.as_view(), name='queue_entry_detail'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='verify'),
-    path('login/', login_page, name='login_page'),
-    path('api/login/', LoginView.as_view(), name='login_api'),
-    path('logout/', logout_view, name='logout'),
-    path('delete_queue/<str:queue_id>/', views.delete_queue, name='delete_queue'),
+    path('register/', views.Register.as_view(), name='register_api'),
+    path('login/', views.LoginView.as_view(), name='login_api'),
+    path('profile/', views.user_profile, name='user_profile'),
+    path('monoinfo/<str:data>/', views.MonoData.as_view(), name='mono_data'),
+    path('', views.home, name='home'),
 ]
